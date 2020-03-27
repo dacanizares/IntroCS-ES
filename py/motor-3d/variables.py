68070d67ad2d13412a975c1d78d49c64e3d34b59
dacanizares <dacanizares@outlook.com>
2020-03-26 22:24:31 -0500

    Added motor variables.py

---
+++ py/motor-3d/variables.py
@@ -0,0 +1,17 @@
+# Posicion del jugador
+jugador_x = 0
+jugador_y = 0
+
+# Direccion
+jugador_rot = 0
+
+# Velocidad
+velocidad = 0
+velocidad_rot = 0
+
+# Para un FOV de 90 grados aprox.
+FOV = 3.14159 / 4.0
+
+# Resolucion
+pantalla_ancho = 400
+pantalla_alto = 100
