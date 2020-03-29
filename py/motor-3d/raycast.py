c14fc5bc164b1a0b62d42b45ee8cbcdab5b6bd44
dacanizares <dacanizares@outlook.com>
2020-03-28 21:51:28 -0500

    Updated raycast

--- py/motor-3d/raycast.py
+++ py/motor-3d/raycast.py
@@ -1,9 +1,10 @@
-distancia = 0
-golpe_muro = False
-while not golpe_muro and distancia <= DISTANCIA_MAX_RAYO: 
+def raycast(x, y, rot):
+  distancia = 0
+  while distancia < DISTANCIA_MAX_RAYO: 
     distanceToWall += 0.1
-    rayo_x = math.floor(jugador_x + math.sin(jugador_rot) * distancia)
-    rayo_y = math.floor(jugador_y + math.cos(jugador_rot) * distancia)
+    rayo_x = math.floor(x + math.sin(rot) * distancia)
+    rayo_y = math.floor(y + math.cos(rot) * distancia)
 
     if toca_muro(rayo_x, rayo_y):
-        golpe_muro = True
+      return distancia  
+  return distancia
