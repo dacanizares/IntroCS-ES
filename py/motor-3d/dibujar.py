7b1f77f843ed748bade2566634c1f9bcc923a5b3
dacanizares <dacanizares@outlook.com>
2020-03-29 20:40:08 -0500

    Updated dibujar

--- py/motor-3d/dibujar.py
+++ py/motor-3d/dibujar.py
@@ -1,16 +1,16 @@
-
+MITAD = PANTALLA_ALTO / 2
 # Dibujamos el techo
-dibujar_rectangulo(0, 0, PANTALLA_ANCHO, PANTALLA_ALTO / 2, (0, 0, 0))
+dibujar_rectangulo(0, 0, PANTALLA_ANCHO, MITAD, (0, 0, 0))
 # Dibujamos el piso
-dibujar_rectangulo(0, PANTALLA_ALTO / 2, PANTALLA_ANCHO, PANTALLA_ALTO / 2, (170, 85, 0))
-
+dibujar_rectangulo(0, MITAD, PANTALLA_ANCHO, MITAD, (170, 85, 0))
 # ...
 
 for pixel in range(0, PANTALLA_ANCHO):
+  # Calculos de las distancias con raycasts
   # ...
 
   # Pintamos el fragmento de muro
-  techo = PANTALLA_ALTO / 2.0 - (PANTALLA_ALTO / distancia)
+  techo = PANTALLA_ALTO / 2.0 - (PANTALLA_ALTO * (distancia / DISTANCIA_MAX_RAYO))
   piso = techo
   shading = max(0, 1 - distancia / DISTANCIA_MAX_RAYO)
   dibujar_rectangulo(pixel, techo, 1, PANTALLA_ALTO - techo - piso, (0, 255 * shading, 0))
