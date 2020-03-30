8867f5e1367bc9ffcaaf3e12110233df584e1d36
dacanizares <dacanizares@outlook.com>
2020-03-29 20:40:43 -0500

    Added techo-piso draw

---
+++ py/motor-3d/techo-piso.py
@@ -0,0 +1,5 @@
+MITAD = PANTALLA_ALTO / 2
+# Dibujamos el techo
+dibujar_rectangulo(0, 0, PANTALLA_ANCHO, MITAD, (0, 0, 0))
+# Dibujamos el piso
+dibujar_rectangulo(0, MITAD, PANTALLA_ANCHO, MITAD, (170, 85, 0))
