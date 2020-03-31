c398860830ed4333cb1718e6527cfc397f8970b7
dacanizares <dacanizares@outlook.com>
2020-03-30 19:00:09 -0500

    Minor improvements

--- py/motor-3d/dibujar.py
+++ py/motor-3d/dibujar.py
@@ -10,7 +10,8 @@ for pixel in range(0, PANTALLA_ANCHO):
   # ...
 
   # Pintamos el fragmento de muro
-  techo = PANTALLA_ALTO / 2.0 - (PANTALLA_ALTO * (distancia / DISTANCIA_MAX_RAYO))
+  techo = max(0, PANTALLA_ALTO / 2.0 - PANTALLA_ALTO / distancia)
   piso = techo
+  muro = PANTALLA_ALTO - techo - piso
   shading = max(0, 1 - distancia / DISTANCIA_MAX_RAYO)
-  dibujar_rectangulo(pixel, techo, 1, PANTALLA_ALTO - techo - piso, (0, 255 * shading, 0))
+  dibujar_rectangulo(pixel, techo, 1, muro, (0, 255 * shading, 0))
