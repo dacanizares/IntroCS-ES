3f6a67b370da9bb0a91d6a5be020d9c53c966073
dacanizares <dacanizares@outlook.com>
2020-04-02 23:54:05 -0500

    Added base code

---
+++ py/pygame/iniciar.py
@@ -0,0 +1,17 @@
+import pygame
+
+# Pantalla
+PANTALLA_ANCHO = 320
+PANTALLA_ALTO = 240
+
+# Pygame
+pygame.init()
+pantalla = pygame.display.set_mode((PANTALLA_ANCHO, PANTALLA_ALTO))
+# Para pantanlla completa
+# pygame.display.set_mode((PANTALLA_ANCHO, PANTALLA_ALTO), pygame.FULLSCREEN)
+
+# Gameloop
+while True:
+  # Aqui iria nuestra logica
+  # ...
+  
